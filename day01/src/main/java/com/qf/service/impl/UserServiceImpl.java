60a6e34c0eb8c829cfe41014bbbacc006e8076f2
albert <albert@gmail.com>
2024-07-29 06:55:17 +0800

    second commit

--- day01/src/main/java/com/qf/service/impl/UserServiceImpl.java
+++ day01/src/main/java/com/qf/service/impl/UserServiceImpl.java
@@ -26,4 +26,8 @@ public class UserServiceImpl implements UserService, InitializingBean {
     public void destroy(){
         System.out.println("destroy...");
     }
+
+    public void test(){
+        System.out.println("test");
+    }
 }
